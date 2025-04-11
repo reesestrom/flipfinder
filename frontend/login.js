ce06781fab6d8b10a20610d752601294e6dbae4c
reesestrom <reesestrom@gmail.com>
2025-04-10 21:46:45 -0600

    Just Flip Finder

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  React.createElement(App, { authOnly: false }) // forces login/signup view
-);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    React.createElement(App, { authOnly: false })
+  );
+  
